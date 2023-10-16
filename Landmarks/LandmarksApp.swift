51d50b4f3b19263c77501b9b4809d19833be4291
SIRIUS KIM <jmoumnet@gmail.com>
2023-10-16 15:51:54 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by yanadoo on 2023/10/16.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
